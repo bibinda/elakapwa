af808773cbdd85c6244f1925d7d73c6916d49d71
Shahriar Ahmed <ahmed.shahriar@gmail.com>
2021-10-11 22:14:18 -0400

    Add English content.

---
+++ public/js/post.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', init, false);
+let requestURL = '../js/messages.json';
+let request = new XMLHttpRequest()
+request.open('GET', requestURL)
+request.responseType = 'json'
+request.send()
+request.onload = function() {
+    const messages = request.response
+    let randomIndex = Math.floor(Math.random()*messages.length)
+    let message = messages[randomIndex]
+    //console.log(message)
+    const intervElem = document.querySelector('.intervention')
+    const genElem = document.querySelector('.general_audience')
+    const tarElem = document.querySelector('.target_audience')
+    const promElem = document.querySelector('.promote')
+    const msgElem = document.querySelector('.message')
+    intervElem.innerHTML = message.intervention
+    genElem.innerHTML = message.general_audience
+    tarElem.innerHTML = message.target_audience
+    promElem.innerHTML = message.promoted_behavior
+    msgElem.innerHTML = message.message.English
+}
+
+function init() {
+}
