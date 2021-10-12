f6d2b5b331b194c886939ecd9c5723a485ba5b62
Shahriar Ahmed <ahmed.shahriar@gmail.com>
2021-10-11 22:37:38 -0400

    Remove blank messages

--- public/js/post.js
+++ public/js/post.js
@@ -9,11 +9,13 @@ request.onload = function() {
     let randomIndex = Math.floor(Math.random()*messages.length)
     let message = messages[randomIndex]
     //console.log(message)
+    const idElem = document.querySelector('.message_id')
     const intervElem = document.querySelector('.intervention')
     const genElem = document.querySelector('.general_audience')
     const tarElem = document.querySelector('.target_audience')
     const promElem = document.querySelector('.promote')
     const msgElem = document.querySelector('.message')
+    idElem.innerHTML = message.id
     intervElem.innerHTML = message.intervention
     genElem.innerHTML = message.general_audience
     tarElem.innerHTML = message.target_audience
